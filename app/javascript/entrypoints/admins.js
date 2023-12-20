1e7d0c9741b25d6a75570345ba16b200e5ae234d
izhanov <ibekable@gmail.com>
2023-12-20 21:34:38 +0600

    Initial commit

---
+++ app/javascript/entrypoints/admins.js
@@ -0,0 +1,14 @@
+import * as Turbo from '@hotwired/turbo-rails'
+window.Turbo = Turbo;
+
+// import ActiveStorage from '@rails/activestorage'
+// ActiveStorage.start()
+//
+// // Import all channels.
+// const channels = import.meta.globEager('./**/*_channel.js')
+
+// Example: Import a stylesheet in app/frontend/index.css
+// import '~/index.css'
+
+import '../src/admins/controllers/index.js';
+
---
+++ app/javascript/entrypoints/application.js
@@ -0,0 +1,11 @@
+import * as Turbo from '@hotwired/turbo-rails'
+window.Turbo = Turbo;
+
+// import ActiveStorage from '@rails/activestorage'
+// ActiveStorage.start()
+//
+// // Import all channels.
+// const channels = import.meta.globEager('./**/*_channel.js')
+
+// Example: Import a stylesheet in app/frontend/index.css
+// import '~/index.css'
