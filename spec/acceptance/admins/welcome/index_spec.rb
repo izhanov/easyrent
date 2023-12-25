43680cee41cec3fe7009ff7d64bf48971a5f28d3
izhanov <ibekable@gmail.com>
2023-12-25 15:07:14 +0600

    ER-1 Add catalogues acceptance test

--- spec/acceptance/admins/welcome/index_spec.rb
+++ spec/acceptance/admins/welcome/index_spec.rb
@@ -22,12 +22,21 @@ feature "Admin visit welcome page" do
     expect(page).to have_selector("h1", text: "Users")
   end
 
-  scenario "and click to Car parks link" do
+  scenario "and click to Cities link" do
     sign_in admin
 
     visit admins_root_path
     click_link "Cities"
     expect(page).to have_current_path(admins_cities_path)
     expect(page).to have_selector("h1", text: "Cities")
+  end
+
+  scenario "and click Catalogues link" do
+    sign_in admin
+
+    visit admins_root_path
+    click_link "Catalogues"
+    expect(page).to have_current_path(admins_catalogues_path)
+    expect(page).to have_selector("h1", text: "Catalogues")
   end
 end
