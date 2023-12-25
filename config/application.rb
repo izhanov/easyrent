688d9ac00e9c1732e76e3c0a843318209a093db1
izhanov <ibekable@gmail.com>
2023-12-25 21:26:29 +0600

    ER-1 Disable action controller include all helpers

--- config/application.rb
+++ config/application.rb
@@ -38,6 +38,7 @@ module Easyrent
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.action_controller.include_all_helpers = false
 
     config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
     config.i18n.available_locales = %i[en ru]
