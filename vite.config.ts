d490d0fad4f15ad545fbcb5d0903e04157b92171
izhanov <ibekable@gmail.com>
2024-02-07 16:44:18 +0600

    Add Vite build script

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite';
-import RubyPlugin from 'vite-plugin-ruby';
-import FullReload from 'vite-plugin-full-reload';
-import StimulusHMR from 'vite-plugin-stimulus-hmr';
 import ViteRails from 'vite-plugin-rails';
 
 export default defineConfig({
   plugins: [
-    RubyPlugin(),
-    StimulusHMR(),
-    FullReload(['app/views/**/*.erb']),
     ViteRails()
   ]
 })
