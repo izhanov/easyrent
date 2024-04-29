f8d7dad51cbd7452bf49af31ef3f2c032a32d47c
izhanov <ibekable@gmail.com>
2024-04-29 14:08:19 +0500

    Add smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,4 +95,13 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.mail.ru",
+    :port                 => 465,
+    :user_name            => ENV["MAIL_USERNAME"],
+    :password             => ENV["MAIL_PASSWORD"],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
 end
